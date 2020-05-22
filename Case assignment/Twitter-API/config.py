eadafa58f56666c02e14a4422497756d118db31c
margomjoy <59444017+margomjoy@users.noreply.github.com>
2020-05-22 11:05:21 -0400

    Add files via upload

---
+++ Case assignment/Twitter-API/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "Sdwm7Vd7tFXqEZYm6Puj7AkIW"
+consumer_secret = "dghwbhsenq3g4MR1qNikCNV3BxVUqAJZsNnuf8Kmxrzftrq6pq"
+access_token = "1240619798138060802-uKFYra8iUWr3qgMifC8LPPj9EJNRGO"
+access_token_secret = "yq1scVy2J9wPf3I87bDVe8Ok2yn9UkCou8jj0f9WejHJJ"
---
+++ Case assignment/Weather-API/starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "ed9ab2e28cee00874214ba2e2f1068a1"
+
+# Google API Key
+g_key = "AIzaSyC1Q915dG3hjogb6awkS9DNB5nqFVmtBcY"
